09a04a430dbd36a0713175e6b657ccfcfbdebb99
Ing. ABV <alexanderbarajas@gmail.com>
2022-11-24 23:07:05 -0500

    Ejercicio de logica para prueba de

---
+++ test.js
@@ -0,0 +1,18 @@
+/* global test, expect */
+/*
+const translate = require('./index.js')
+
+test('it translates "Hello" accordingly', ()=>{
+    expect(
+        translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊')
+    ).ToBe('Hello')
+})
+
+
+test.skip('it translates "Hello World!" accordingly', ()=>{
+    expect(
+        translate('👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊')
+    ).ToBe('Hello World!')
+})
+
+*/
